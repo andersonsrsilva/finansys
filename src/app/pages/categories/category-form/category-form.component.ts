c5a65b63525e25f673cc4f1a2baffc2e39617520
andersonsrsilva@gmail.com <andersonsrsilva@gmail.com>
2019-01-19 11:36:22 -0200

    improving category form

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, Routes, Router } from '@angular/router';
 
 import { switchMap } from 'rxjs/operators';
 import { Category } from '../shared/category.model';
+import toasts from 'toastr';
 
 @Component({
   selector: 'app-category-form',
@@ -14,7 +15,7 @@ import { Category } from '../shared/category.model';
 export class CategoryFormComponent implements OnInit {
 
   currentAction: string;
-  categortForm: FormGroup;
+  categoryForm: FormGroup;
   pageTitle: string;
   serverErrorMessages: string[] = null;
   submittingForm = false;
@@ -35,7 +36,59 @@ export class CategoryFormComponent implements OnInit {
 
   ngAfterContentChecked() {
     this.setPageTitle();
+  }
 
+  get f() {
+    return this.categoryForm.controls;
+  }
+
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction === 'new') {
+      this.createCategory();
+    } else {
+      this.updateCategory();
+    }
+  }
+
+  private createCategory() {
+    const categoryForm: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(categoryForm)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      );
+  }
+
+  private updateCategory() {
+    const categoryForm: Category = Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.update(categoryForm)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    );
+  }
+
+  private actionsForSuccess(category: Category) {
+    toasts.success('Solicitação processada com sucesso.');
+
+    this.router.navigateByUrl('categories', { skipLocationChange: true }).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    );
+  }
+
+  private actionsForError(error) {
+    toasts.console.error('Ocorreu um erro ao processar sua solicitação.');
+
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor'];
+    }
   }
 
   private setCurrentAction() {
@@ -47,7 +100,7 @@ export class CategoryFormComponent implements OnInit {
   }
 
   private buildCategoryForm() {
-    this.categortForm = this.formBuilder.group({
+    this.categoryForm = this.formBuilder.group({
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null]
@@ -56,24 +109,24 @@ export class CategoryFormComponent implements OnInit {
 
   private loadCategory() {
     if (this.currentAction === 'edit') {
-        this.activatedRoute.paramMap.pipe(
-          switchMap(params => this.categoryService.getById(+params.get('id')))
-        ).subscribe(
-          (category) => {
-            this.category = category;
-            this.categortForm.patchValue(category);
-          },
-          (error) => alert('Ocorreu um erro')
-        )
+      this.activatedRoute.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get('id')))
+      ).subscribe(
+        (category) => {
+          this.category = category;
+          this.categoryForm.patchValue(category);
+        },
+        (error) => alert('Ocorreu um erro')
+      );
     }
   }
 
   private setPageTitle() {
     if (this.currentAction === 'new') {
-        this.pageTitle = 'Cadastro de Nova Categoria';
+      this.pageTitle = 'Cadastro de Nova Categoria';
     } else {
-        const categoryName = this.category.name || '';
-        this.pageTitle = 'Editando Categoria: ' + categoryName;
+      const categoryName = this.category.name || '';
+      this.pageTitle = 'Editando Categoria: ' + categoryName;
     }
   }
 
