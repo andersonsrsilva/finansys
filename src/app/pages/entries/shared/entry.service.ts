3b9252c834e9f1729cd64530dc005df36534eb6f
andersonsrsilva@gmail.com <andersonsrsilva@gmail.com>
2019-02-02 19:36:39 -0200

    Improving Entries Module

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -5,6 +5,8 @@ import { Observable, throwError } from 'rxjs';
 import { map, catchError, flatMap } from 'rxjs/operators';
 
 import { Entry } from './entry.model';
+import { CategoryService } from '../../categories/shared/category.service';
+import { Category } from '../../categories/shared/category.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,10 @@ export class EntryService {
 
   private apiPath = 'api/entries';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private categoryService: CategoryService
+    ) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe (
@@ -32,18 +37,31 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, Entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
+    console.log('dfg');
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        );
+      })
     );
   }
 
   update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
 
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        );
+      })
     );
   }
 
