fa67a0888236ba9b77abedd822434f99a70ada1b
andersonsrsilva@gmail.com <andersonsrsilva@gmail.com>
2019-02-23 13:17:05 -0300

    Refactoring navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,24 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   exports : [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    NavbarComponent
   ]
 })
 export class CoreModule { }
